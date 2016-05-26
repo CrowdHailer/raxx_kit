5a57cc83b8bb7b37761e7b5ca605268510882341
CrowdHailer <peter@goodlord.co>
2016-05-26 17:12:59 +0100

    test the plug

--- test/baobab_test.exs
+++ test/baobab_test.exs
@@ -1,8 +1,14 @@
 defmodule BaobabTest do
-  use ExUnit.Case
-  doctest Baobab
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "root page returns 200" do
+    opts = Baobab.RootController.init(%{})
+    conn = conn(:get, "/")
+    conn = Baobab.RootController.call(conn, opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert String.contains?(conn.resp_body, "Hello")
   end
 end
