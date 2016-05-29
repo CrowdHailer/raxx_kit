353ed0774957f3b24d69413c94fc9c2fce3e2186
CrowdHailer <peterhsaxton@gmail.com>
2016-05-29 19:22:20 +0100

    remove supervisor

--- lib/baobab.ex
+++ lib/baobab.ex
@@ -12,21 +12,7 @@ defmodule Baobab do
   end
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Baobab.Worker, [arg1, arg2, arg3]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Baobab.Supervisor]
-    Supervisor.start_link(children, opts)
-
     Plug.Adapters.Cowboy.http(RootController, %{})
   end
 end
