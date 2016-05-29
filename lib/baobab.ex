7ea0dd67619d1dbcf1e4ee2d50fcce48f259e867
CrowdHailer <peterhsaxton@gmail.com>
2016-05-29 20:42:02 +0100

    push template logic to module TODO macro for file rendering

--- lib/baobab.ex
+++ lib/baobab.ex
@@ -1,13 +1,4 @@
-defmodule RootPage do
-  require EEx
-
-  template_file = String.replace_suffix(__ENV__.file, ".ex", ".html.eex")
-  EEx.function_from_file :defp, :render_template, template_file, [:assigns]
-
-  def render(assigns) do
-    render_template(assigns)
-  end
-
+defmodule HtmlView do
   @escapes [
     {?<, "&lt;"},
     {?>, "&gt;"},
@@ -26,17 +17,37 @@ defmodule RootPage do
     IO.iodata_to_binary(for <<char <- buffer>>, do: escape_char(char))
   end
 end
+
+defmodule RootPage do
+  require EEx
+
+  template_file = String.replace_suffix(__ENV__.file, ".ex", ".html.eex")
+  EEx.function_from_file :defp, :render_template, template_file, [:assigns]
+
+  def html(assigns) do
+    assigns
+    |> Enum.map(&escape_assigned_values/1)
+    |> Enum.into(%{})
+    |> render_template
+  end
+
+  defp escape_assigned_values({key, value}) do
+    {key, HtmlView.escape(value)}
+  end
+
+end
 defmodule Baobab do
   defmodule RootController do
     import Plug.Conn
 
     def init(opts) do
-      Map.put(opts, :greeting, "Hello")
+      opts
+      |> Map.put(:greeting, "Hello")
+      |> Map.put(:other, "<h1>&</h1>")
     end
 
     def call(conn, opts) do
-      IO.inspect(RootPage.escape("<h1>&</h1>"))
-      page = RootPage.render(opts)
+      page = RootPage.html(opts)
       IO.inspect(page)
       send_resp(conn, 200, page)
     end
