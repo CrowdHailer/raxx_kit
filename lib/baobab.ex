c3cc1c48f5e0fabe28113bb7d11211d9166c4cf0
CrowdHailer <peterhsaxton@gmail.com>
2016-05-30 15:35:43 +0100

    separate view module

--- lib/baobab.ex
+++ lib/baobab.ex
@@ -1,41 +1,7 @@
-defmodule HtmlView do
-  @escapes [
-    {?<, "&lt;"},
-    {?>, "&gt;"},
-    {?&, "&amp;"},
-    {?", "&quot;"},
-    {?', "&#39;"}
-  ]
-
-  Enum.each @escapes, fn { match, insert } ->
-    defp escape_char(unquote(match)), do: unquote(insert)
-  end
-
-  defp escape_char(char), do: << char >>
-
-  def escape(buffer) do
-    IO.iodata_to_binary(for <<char <- buffer>>, do: escape_char(char))
-  end
-end
-
 defmodule RootPage do
-  require EEx
-
-  template_file = String.replace_suffix(__ENV__.file, ".ex", ".html.eex")
-  EEx.function_from_file :defp, :render_template, template_file, [:assigns]
-
-  def html(assigns) do
-    assigns
-    |> Enum.map(&escape_assigned_values/1)
-    |> Enum.into(%{})
-    |> render_template
-  end
-
-  defp escape_assigned_values({key, value}) do
-    {key, HtmlView.escape(value)}
-  end
-
+  use HtmlView
 end
+
 defmodule Baobab do
   defmodule RootController do
     import Plug.Conn
