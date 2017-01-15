79f9daa9e02c0340fac9061cb6f8ccb3a8d69455
CrowdHailer <peterhsaxton@gmail.com>
2017-01-15 15:00:32 +0000

    matching on static strings

--- router/lib/router.ex
+++ router/lib/router.ex
@@ -1,2 +1,15 @@
 defmodule Tokumei.Router do
+  defmacro route(path, do: clauses) do
+    path = Raxx.Request.split_path(path)
+    request_match = quote do: %{path: unquote(path)}
+    quote do
+      def handle_request(request = unquote(request_match), env) do
+        unquote(Macro.var(:request, nil)) = request
+        unquote(Macro.var(:env, nil)) = env
+        case request.method do
+          unquote(clauses)
+        end
+      end
+    end
+  end
 end
--- router/test/router_test.exs
+++ router/test/router_test.exs
@@ -1,8 +1,43 @@
 defmodule Tokumei.RouterTest do
   use ExUnit.Case
-  doctest Tokumei.Router
+  alias Raxx.Request
+  alias Raxx.Response
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Tokumei.Router
+
+  route "foo" do
+    :GET -> Response.ok("foo")
+    :POST -> Response.no_content()
+  end
+
+  test "Will match on a single path element" do
+    %{status: status, body: body} = get("/foo")
+    assert 200 == status
+    assert "foo" == body
+  end
+
+  test "Will match on any method single path element" do
+    %{status: status, body: body} = post("/foo")
+    assert 204 == status
+  end
+
+  route "bar" do
+    :GET ->
+      IO.inspect(request)
+      Response.ok()
+  end
+
+  test "Will handle multiple lines single path element" do
+    assert 200 == get("/bar").status
+  end
+
+  defp get(path) do
+    Request.get(path)
+    |>  __MODULE__.handle_request(:empty_env)
+  end
+
+  defp post(path) do
+    Request.post(path)
+    |>  __MODULE__.handle_request(:empty_env)
   end
 end
