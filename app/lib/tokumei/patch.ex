4ef55dbcc7b5428ad2b8ca8a7382527d65108486
CrowdHailer <peterhsaxton@gmail.com>
2017-04-08 10:36:52 +0100

    create redirect/2 and back/2 helpers

--- app/lib/tokumei/patch.ex
+++ app/lib/tokumei/patch.ex
@@ -2,9 +2,6 @@ defmodule Tokumei.Patch do
   @moduledoc """
   Home of functionality that should be contributed to the Raxx project
   """
-  def redirect(path) do
-    Raxx.Response.found([{"location", path}])
-  end
 
   def request_to_url(%{host: host, port: port, path: path, mount: mount, scheme: scheme}) do
     %URI{
---
+++ app/test/tokumei/helpers_test.exs
@@ -0,0 +1,7 @@
+defmodule Tokumei.HelpersTest do
+  use ExUnit.Case
+
+  alias Raxx.Request
+  alias Tokumei.Helpers
+  doctest Tokumei.Helpers
+end
