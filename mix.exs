e4bc62110fa43dee6a9561165f52f86bda453e7f
CrowdHailer <peterhsaxton@gmail.com>
2018-02-11 19:23:05 +0000

    release archive for 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RaxxKit.MixProject do
   def project do
     [
       app: :raxx_kit,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
