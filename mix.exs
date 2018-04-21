4c8dbddb708bd9b2ae205208383cbc6372c4a74e
CrowdHailer <peterhsaxton@gmail.com>
2018-04-21 16:57:48 +0100

    add project information to publish on hex

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule RaxxKit.MixProject do
   def project do
     [
       app: :raxx_kit,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      docs: [extras: ["README.md"], main: "readme"],
+      package: package()
     ]
   end
 
@@ -18,6 +21,22 @@ defmodule RaxxKit.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Framework for web applications with Raxx and Ace
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Peter Saxton"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/crowdhailer/raxx_kit"}
+    ]
   end
 end
