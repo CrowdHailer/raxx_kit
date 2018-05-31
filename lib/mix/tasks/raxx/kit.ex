0dc98042d02aa717c5f07dc3555bf4212d0e81b9
Jacek Królikowski <nietaki@gmail.com>
2018-06-01 00:15:44 +0200

    Add moduledoc for mix raxx.kit task

--- lib/mix/tasks/raxx/kit.ex
+++ lib/mix/tasks/raxx/kit.ex
@@ -3,8 +3,37 @@ defmodule Mix.Tasks.Raxx.Kit do
 
   require Mix.Generator
 
-  @shortdoc "Create a new Raxx project for browsers"
+  @shortdoc "Creates a new Raxx project for browsers"
   @switches [docker: :boolean, apib: :boolean, module: :string, node_assets: :boolean]
+
+  @moduledoc """
+  Creates a new Raxx project for browsers.
+
+  It expects the name of the project as the argument.
+
+      mix raxx.kit NAME [--node-assets] [--docker] [--module ModuleName] [--apib]
+
+  ## Options
+
+  - `--node-assets`: Add JavaScript compilation as part of a generated project.
+    Works with or without docker.
+
+  - `--docker`: Create `Dockerfile` and `docker-compose.yml` in template.
+    This allows local development to be conducted completly in docker.
+
+  - `--module`: Used to name the top level module used in the generated project.
+  Without this option the module name will be generated from path option.
+
+  ```sh
+  $ mix raxx.kit my_app
+
+  # Is equivalent to
+  $ mix raxx.kit my_app --module MyApp
+  ```
+
+  - `--apib`: Generate an API Blueprint file which is used as the project router.
+
+  """
 
   @impl Mix.Task
   def run([]) do
