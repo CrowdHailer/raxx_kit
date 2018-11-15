686c55b2652ba1f7aef6ffab420b2555984004f2
Jacek Królikowski <nietaki@gmail.com>
2018-11-15 13:19:42 +0000

    More comprehensive travis docker tests.

--- scripts/docker_test.sh
+++ scripts/docker_test.sh
@@ -16,22 +16,30 @@ popd
 pushd $DIR/../demo
 
 echo ""
-echo "## getting demo project dependencies inside the container"
-echo ""
-
-docker-compose run demo mix deps.get
-
-echo ""
 echo "## starting all services"
 echo ""
 
 docker-compose up -d
 
 echo ""
+echo "## getting demo project dependencies inside the container"
+echo ""
+
+docker-compose run demo mix deps.get
+
+echo ""
 echo "## running tests for demo project inside the container"
 echo ""
 
 docker-compose run demo mix test
+
+echo ""
+echo "## make sure the demo service is still alive"
+echo ""
+
+# if the demo project has problems starting, the service
+# will probably be dead by now
+docker-compose exec demo echo "I'm still alive!"
 
 # cleanup, for local docker_test runs
 docker-compose down
